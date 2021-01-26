00a08634d7de7ed1908f7b80ae88b5e04d64a240
René de Hesselle <dehesselle@web.de>
2021-01-26 22:32:36 +0100

    Add build log to release

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+### Tcl version ################################################################
+
 TCL_VER_MAJOR=8
 TCL_VER_MINOR=6
 TCL_VER_PATCH=11
@@ -7,32 +9,57 @@ TCL_VER=$TCL_VER_MAJOR.$TCL_VER_MINOR
 TCL_VER_FULL=$TCL_VER.$TCL_VER_PATCH
 TCL_URL=https://prdownloads.sourceforge.net/tcl/tcl$TCL_VER_FULL-src.tar.gz
 
-#########
+### work directory #############################################################
 
 if [ -z $WRK_DIR ]; then
   WRK_DIR=$(mktemp -d /Users/Shared/work/tcl.XXXXXX)
 fi
 
-export PATH=/Users/Shared/work/bin:/usr/bin:/bin:/usr/sbin:/sbin
+### path #######################################################################
+
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
+### target platform ############################################################
 
 if [ -z $SDKROOT ]; then
   SDKROOT=$(xcrun --show-sdk-path)
 fi
 export SDKROOT
-export MACOSX_DEPLOYMENT_TARGET=$(/usr/libexec/PlistBuddy -c 'Print :DefaultProperties:MACOSX_DEPLOYMENT_TARGET' $SDKROOT/SDKSettings.plist)
 
-##########
+# get deployment target version from SDK
+export MACOSX_DEPLOYMENT_TARGET=$(/usr/libexec/PlistBuddy \
+  -c 'Print :DefaultProperties:MACOSX_DEPLOYMENT_TARGET' \
+  $SDKROOT/SDKSettings.plist)
+
+### build ######################################################################
+
+LOG=$WRK_DIR/build.log
 
 mkdir -p $WRK_DIR
 cd $WRK_DIR
+
+echo "SDKROOT=$SDKROOT" >> $LOG
+echo "MACOSX_DEPLOYMENT_TARGET=$MACOSX_DEPLOYMENT_TARGET" >> $LOG
+
 curl -L $TCL_URL | tar xz
 cd tcl*/unix
+
 ./configure --prefix=$WRK_DIR --enable-64bit --enable-framework
 make -j$(sysctl -n hw.ncpu)
-install_name_tool -change /Library/Frameworks/Tcl.framework/Versions/$TCL_VER/Tcl @executable_path/Tcl tclsh
+install_name_tool \
+  -change /Library/Frameworks/Tcl.framework/Versions/$TCL_VER/Tcl \
+  @executable_path/Tcl \
+  tclsh
 make install DESTDIR=$WRK_DIR NATIVE_TCLSH=$(pwd)/tclsh
 
-cp tclsh $WRK_DIR/Library/Frameworks/Tcl.framework/Versions/$TCL_VER/tclsh$TCL_VER
+cp tclsh \
+  $WRK_DIR/Library/Frameworks/Tcl.framework/Versions/$TCL_VER/tclsh$TCL_VER
 
-tar -C $WRK_DIR/Library/Frameworks -cjf $WRK_DIR/Tcl.framework.tar.bz2 Tcl.framework
-tar -C $WRK_DIR/Library -cjf $WRK_DIR/Tcl.tar.bz2 Tcl
+### packages artifacts #########################################################
+
+tar -C $WRK_DIR/Library/Frameworks \
+  -cjf $WRK_DIR/Tcl.framework.tar.bz2 \
+  Tcl.framework
+tar -C $WRK_DIR/Library \
+  -cjf $WRK_DIR/Tcl.tar.bz2 \
+  Tcl
