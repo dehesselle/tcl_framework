51b3d80f130cad72dca22c4976a8e837a6a5c617
René de Hesselle <dehesselle@web.de>
2021-01-26 19:57:39 +0100

    Add CI

--- build.sh
+++ build.sh
@@ -9,11 +9,12 @@ TCL_URL=https://prdownloads.sourceforge.net/tcl/tcl$TCL_VER_FULL-src.tar.gz
 
 #########
 
-WRK_DIR=$(mktemp -d /Users/Shared/work/tcl.XXXXXX)
-
-SDKROOT=/opt/sdks/MacOSX10.11.sdk
+if [ -z $WRK_DIR ]; then
+  WRK_DIR=$(mktemp -d /Users/Shared/work/tcl.XXXXXX)
+fi
 
 export PATH=/Users/Shared/work/bin:/usr/bin:/bin:/usr/sbin:/sbin
+
 if [ -z $SDKROOT ]; then
   SDKROOT=$(xcrun --show-sdk-path)
 fi
@@ -22,6 +23,7 @@ export MACOSX_DEPLOYMENT_TARGET=$(/usr/libexec/PlistBuddy -c 'Print :DefaultProp
 
 ##########
 
+mkdir -p $WRK_DIR
 cd $WRK_DIR
 curl -L $TCL_URL | tar xz
 cd tcl*/unix
@@ -31,3 +33,6 @@ install_name_tool -change /Library/Frameworks/Tcl.framework/Versions/$TCL_VER/Tc
 make install DESTDIR=$WRK_DIR NATIVE_TCLSH=$(pwd)/tclsh
 
 cp tclsh $WRK_DIR/Library/Frameworks/Tcl.framework/Versions/$TCL_VER/tclsh$TCL_VER
+
+tar -C $WRK_DIR/Library/Frameworks -cjf $WRK_DIR/Tcl.framework.tar.bz2 Tcl.framework
+tar -C $WRK_DIR/Library -cjf $WRK_DIR/Tcl.tar.bz2 Tcl
